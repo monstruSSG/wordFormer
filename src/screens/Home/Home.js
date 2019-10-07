dcbde584f5cd50c7bf486080a3d3bd5417d0d497
monstruSSG <b_giurgi@yahoo.ro>
2019-10-07 22:55:03 +0300

    Almost done with homescreen

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image, ImageBackground } from 'react-native';
 
 import BaseScreen from '../../components/BaseScreen/BaseScreen';
+import CustomText from '../../components/UI/CustonText';
+
+import Logo from '../../assets/logo.png';
+import PlayButton from '../../assets/greenButton.png';
+import FbButton from '../../assets/fbButton.png';
+import ProfilePic from '../../assets/radeCarte.png';
 
 class Home extends Component {
     static navigationOptions = {
@@ -11,14 +17,75 @@ class Home extends Component {
     render() {
         return (
             <BaseScreen>
-                <View>
-                    <Text>
-                        Home
-                    </Text>
+                <View style={[styles.max, styles.center]}>
+                    <View style={[styles.center, styles.logoContainer]}>
+                        <Image source={Logo} resizeMode='contain' style={styles.logoSize} />
+                    </View>
+                    <View style={[styles.buttonsContainer, styles.buttonsPosition]}>
+                        <View style={[styles.half, { alignItems: 'center', justifyContent: 'flex-end' }]}>
+                            <ImageBackground source={PlayButton} style={styles.playButton} resizeMode='contain'>
+                                <View style={[styles.center, styles.playButton]}>
+                                    <CustomText large>PLAY</CustomText>
+                                </View>
+                            </ImageBackground>
+                        </View>
+                        <View style={[styles.half, { alignItems: 'center' }]}>
+                            <ImageBackground source={FbButton} style={styles.playButton} resizeMode='contain'>
+                                <View style={[styles.center, styles.playButton]}>
+                                    <CustomText large>SHARE ON FB</CustomText>
+                                </View>
+                            </ImageBackground>
+                        </View>
+                    </View>
+                    <View style={[styles.bottomContainer, { flexDirection: 'row', justifyContent: 'flex-end' }]}>
+                        <View style={[{ height: '120%', width: '30%'}, styles.center]}>
+                            <Image source={ProfilePic} style={styles.playButton} resizeMode='cover' />
+                        </View>
+                    </View>
                 </View>
             </BaseScreen>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    buttonsPosition: {
+        position:'relative',
+        top: '5%',
+        left: '5%'
+    },
+    playButton: {
+        width: '90%',
+        height: '80%'
+    },
+    half: {
+        width: '100%',
+        height: '50%'
+    },
+    max: {
+        width: '100%',
+        height: '100%'
+    },
+    logoSize: {
+        width: '85%',
+        height: '90%'
+    },
+    center: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    logoContainer: {
+        width: '100%',
+        height: '50%'
+    },
+    buttonsContainer: {
+        width: '100%',
+        height: '35%'
+    },
+    bottomContainer: {
+        width: '100%',
+        height: '15%'
+    }
+});
 
 export default Home;
